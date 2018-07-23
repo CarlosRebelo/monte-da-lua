76a9f092e5d477e48428d4163349ed531e06e555
Carlos Rebelo <CarlosRebelo@github.com>
2018-07-23 12:07:30 +0100

    add content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Card from './Components/Card/Card'
+import Content from './Components/Content/Content'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Card />
+      <div className='background'>
+        <Card />
+        <Content />
+      </div>
     );
   }
 }
